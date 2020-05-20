a662f6faa4f2d1ec07328b407b720364ad94847f
Aditya <adityapratamax11@gmail.com>
2020-05-20 11:54:47 +0700

    setup prod build heroku

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname,'..','public');
+const port = process.env || 3000;
+console.log(path.join(publicPath,'index.html'))
+
+app.use(express.static(publicPath));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(publicPath,'index.html'));
+});
+
+app.listen(port, () => {
+    console.log('server up port',port);
+})
